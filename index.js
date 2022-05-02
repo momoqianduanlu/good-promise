2e85a4e12d29bc384c7daae2f3885980dd8396b3
cgs <215966931@qq.com>
2022-05-02 21:51:59 +0800

    feat: 处理promise中的异步与多次调用的问题

--- index.js
+++ index.js
@@ -2,14 +2,27 @@ const MyPromise = require('./promise')
 
 const promise = new MyPromise((resolve, reject) => {
   // resolve('success')
-  throw new Error('Exception: Error')
+  // throw new Error('Exception: Error')
+
+  setTimeout(() => {
+    resolve('success')
+  }, 3000)
 })
 
 promise.then(
   res => {
-    console.log('res', res)
+    console.log('res1', res)
   },
   err => {
-    console.log('err', err)
+    console.log('err1', err)
+  }
+)
+
+promise.then(
+  res => {
+    console.log('res2', res)
+  },
+  err => {
+    console.log('err2', err)
   }
 )
--- promise.js
+++ promise.js
@@ -12,21 +12,25 @@ class MyPromise {
     this.status = PENDING
     this.value = undefined
     this.error = undefined
+    this.onFullFilledStacks = []
+    this.onRejectedStacks = []
     /**
-     * 2. 为什么 resolve，reject函数不声明在构造函数外面，
+     * 2. 为什么 resolve，reject函数不声明在构造函数外面？
      * 站在es6的角度上来讲，我们将这个两个函数定义在构造函数的外面实际上是定义在了MyPromise的原型上面了，
-     * 这样我们每一个MyPromise都是继承的一个resolve，但是每一个MyPromise里面的 executor 执行器里面都应该有自己的 resolve和reject。
+     * 这样我们每一个MyPromise都是继承的一个resolve，但是每一个MyPromise里面的executor执行器里面都应该有自己的resolve和reject。
      */
     const resolve = value => {
       if (this.status === PENDING) {
         this.status = FULLFILLED
         this.value = value
+        this.onFullFilledStacks.forEach(fn => fn())
       }
     }
     const reject = error => {
       if (this.status === PENDING) {
         this.status = REJECT
         this.error = error
+        this.onRejectedStacks.forEach(fn => fn())
       }
     }
     try {
@@ -42,6 +46,21 @@ class MyPromise {
     }
     if (this.status === REJECT) {
       onRejected(this.error)
+    }
+    /**
+     * 3. 处理异步调用的promise
+     * 同步调用promise的时候会顺序执行promise.then()函数。
+     * 但当异步调用promise的时候，此时promise还处于pending状态，onFullFilled, onRejected并不知道先去执行哪一个，
+     * 通过promise.then先去订阅onFullFilled, onRejected函数，将他们push到一个执行栈里面，
+     * 不管是不是异步调用promise，最终都要执行resolve和reject，所以在这两个函数执行的时候再去发布订阅的函数。
+     */
+    if (this.status === PENDING) {
+      this.onFullFilledStacks.push(() => {
+        onFullFilled(this.value)
+      })
+      this.onRejectedStacks.push(() => {
+        onRejected(this.error)
+      })
     }
   }
 }
